854cea33d59b03b90ede412ec79d7777f77fb848
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-09-20 14:45:09 +0200

    Add support BigInt converting to buffer

--- lib-client/src/messages/out/message.ts
+++ lib-client/src/messages/out/message.ts
@@ -22,9 +22,28 @@ export abstract class Message<T> implements OutgoingMessage {
         const encoder: TextEncoder = new TextEncoder();
         const buffer: Uint8Array = encoder.encode(JSON.stringify(this.getMessage()));
         const id: Uint32Array = Uint32Array.from([this.id]);
-        const ts: BigInt64Array = BigInt64Array.from([BigInt((new Date()).getTime())]);
+        const ts: ArrayBufferLike = this._bigIntToBuff(BigInt((new Date()).getTime()));
         const len: Uint32Array = Uint32Array.from([buffer.buffer.byteLength]);
-        return Tools.append([id.buffer, ts.buffer, len.buffer, buffer.buffer]);
+        return Tools.append([id.buffer, ts, len.buffer, buffer.buffer]);
+    }
+
+    private _bigIntToBuff(int: bigint): ArrayBufferLike {
+        if (window !== undefined && (window as any).BigInt64Array !== undefined) {
+            return (window as any).BigInt64Array.from([int]).buffer;
+        } else {
+            let hex: string = BigInt(int).toString(16);
+            if (hex.length % 2) { hex = '0' + hex; }
+            const len = hex.length / 2;
+            const u8 = new Uint8Array(len);
+            let i = 0;
+            let j = 0;
+            while (i < len) {
+                u8[i] = parseInt(hex.slice(j, j+2), 16);
+                i += 1;
+                j += 2;
+            }
+            return u8;
+        }
     }
 
 }
