dbed5b8608e8c4d5ddce9109443305c0b384612e
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-11-08 14:02:46 +0100

    Support vectors

--- lib/src/connection/connection.message.rs
+++ lib/src/connection/connection.message.rs
@@ -127,7 +127,173 @@ impl Storage {
 
 }
 
-mod StructEncodeDecode {
+mod DecodeTools {
+
+    use std::io::Cursor;
+    use std::mem;
+    use bytes::{Buf};
+    use super::{ Storage };
+
+    const U8_LEN: usize = mem::size_of::<u8>();
+    const U16_LEN: usize = mem::size_of::<u16>();
+    const U32_LEN: usize = mem::size_of::<u32>();
+    const U64_LEN: usize = mem::size_of::<u64>();
+    const I8_LEN: usize = mem::size_of::<i8>();
+    const I16_LEN: usize = mem::size_of::<i16>();
+    const I32_LEN: usize = mem::size_of::<i32>();
+    const I64_LEN: usize = mem::size_of::<i64>();
+    const USIZE_LEN: usize = mem::size_of::<usize>();
+    const F32_LEN: usize = mem::size_of::<f32>();
+    const F64_LEN: usize = mem::size_of::<f64>();
+    const BOOL_LEN: usize = mem::size_of::<bool>();
+
+    pub fn get_u8(storage: &mut Storage, name: String) -> Result<u8, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < U8_LEN {
+                return Err(format!("To extract u8 value buffer should have length at least {} bytes, but length is {}", U8_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_u8())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_u16(storage: &mut Storage, name: String) -> Result<u16, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < U16_LEN {
+                return Err(format!("To extract u16 value buffer should have length at least {} bytes, but length is {}", U16_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_u16_le())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_u32(storage: &mut Storage, name: String) -> Result<u32, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < U32_LEN {
+                return Err(format!("To extract u32 value buffer should have length at least {} bytes, but length is {}", U32_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_u32_le())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_u64(storage: &mut Storage, name: String) -> Result<u64, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < U64_LEN {
+                return Err(format!("To extract u64 value buffer should have length at least {} bytes, but length is {}", U64_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_u64_le())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_i8(storage: &mut Storage, name: String) -> Result<i8, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < I8_LEN {
+                return Err(format!("To extract i8 value buffer should have length at least {} bytes, but length is {}", I8_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_i8())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_i16(storage: &mut Storage, name: String) -> Result<i16, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < I16_LEN {
+                return Err(format!("To extract i16 value buffer should have length at least {} bytes, but length is {}", I16_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_i16_le())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_i32(storage: &mut Storage, name: String) -> Result<i32, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < I32_LEN {
+                return Err(format!("To extract i32 value buffer should have length at least {} bytes, but length is {}", I32_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_i32_le())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_i64(storage: &mut Storage, name: String) -> Result<i64, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < I64_LEN {
+                return Err(format!("To extract i64 value buffer should have length at least {} bytes, but length is {}", I64_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_i64_le())
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_bool(storage: &mut Storage, name: String) -> Result<bool, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            if buf.len() < U8_LEN {
+                return Err(format!("To extract u8 value buffer should have length at least {} bytes, but length is {}", U8_LEN, buf.len()));
+            }
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            Ok(cursor.get_u8() != 0)
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_u8_vec(storage: &mut Storage, name: String) -> Result<Vec<u8>, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            let mut res: Vec<u8> = vec!();
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            loop {
+                if cursor.position() == buf.len() as u64 {
+                    break;
+                }
+                res.push(cursor.get_u8());
+            }
+            Ok(res)
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+    pub fn get_u16_vec(storage: &mut Storage, name: String) -> Result<Vec<u16>, String> {
+        if let Some(buf) = storage.get(name.clone()) {
+            let mut res: Vec<u16> = vec!();
+            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
+            let len = buf.len() as u64;
+            loop {
+                if cursor.position() == len {
+                    break;
+                }
+                if len - cursor.position() < U16_LEN as u64 {
+                    return Err(format!("To extract u16 value from array buffer should have length at least {} bytes, but length is {}", U8_LEN, buf.len()));
+                }
+                res.push(cursor.get_u16_le());
+            }
+            Ok(res)
+        } else {
+            Err(format!("Buffer for property {} isn't found", name))
+        }
+    }
+
+}
+
+mod EncodeTools {
 
     use std::convert::TryFrom;
     use std::mem;
@@ -213,6 +379,23 @@ mod StructEncodeDecode {
         get_value_buffer(name, BOOL_LEN as u32, if value { vec![1] } else { vec![0] })
     }
 
+    pub fn get_string(name: String, value: String) -> Result<Vec<u8>, String> {
+        let buf = value.as_bytes();
+        get_value_buffer(name, buf.len() as u32, buf.to_vec())
+    } 
+
+    pub fn get_u8_vec(name: String, value: Vec<u8>) -> Result<Vec<u8>, String> {
+        get_value_buffer(name, value.len() as u32, value.to_vec())
+    }
+
+    pub fn get_u16_vec(name: String, value: Vec<u16>) -> Result<Vec<u8>, String> {
+        let len = value.len() * U16_LEN;
+        let mut buffer: Vec<u8> = vec!();
+        for val in value.iter() {
+            buffer.append(&mut val.to_le_bytes().to_vec());
+        }
+        get_value_buffer(name, len as u32, buffer.to_vec())
+    }
 
 }
 
@@ -232,27 +415,29 @@ trait StructEncode {
 struct Target {
     pub prop_a: u16,
     pub prop_b: u32,
+    pub prop_c: Vec<u8>,
+    pub prop_d: Vec<u16>,
 }
 
 impl StructDecode for Target {
 
     fn decode(&mut self, mut storage: Storage) -> Result<(), String> {
-        let prop_a: u16;
-        let prop_b: u32;
-        if let Some(buf) = storage.get(String::from("prop_a")) {
-            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
-            prop_a = cursor.get_u16_le();
-        } else {
-            return Err("Buffer for property prop_a isn't found".to_string());
-        }
-        if let Some(buf) = storage.get(String::from("prop_b")) {
-            let mut cursor: Cursor<&[u8]> = Cursor::new(buf);
-            prop_b = cursor.get_u32_le();
-        } else {
-            return Err("Buffer for property prop_b isn't found".to_string());
-        }
-        self.prop_a = prop_a;
-        self.prop_b = prop_b;
+        self.prop_a = match DecodeTools::get_u16(&mut storage, String::from("prop_a")) {
+            Ok(val) => val,
+            Err(e) => { return Err(e) },
+        };
+        self.prop_b = match DecodeTools::get_u32(&mut storage, String::from("prop_b")) {
+            Ok(val) => val,
+            Err(e) => { return Err(e) },
+        };
+        self.prop_c = match DecodeTools::get_u8_vec(&mut storage, String::from("prop_c")) {
+            Ok(val) => val,
+            Err(e) => { return Err(e) },
+        };
+        self.prop_d = match DecodeTools::get_u16_vec(&mut storage, String::from("prop_d")) {
+            Ok(val) => val,
+            Err(e) => { return Err(e) },
+        };
         Ok(())
     }
 
@@ -262,12 +447,20 @@ impl StructEncode for Target {
 
     fn encode(&mut self) -> Result<Vec<u8>, String> {
         let mut buffer: Vec<u8> = vec!();
-        match StructEncodeDecode::get_u16(String::from("prop_a"), self.prop_a) {
-            Ok(mut buf) => { buffer. append(&mut buf); },
+        match EncodeTools::get_u16(String::from("prop_a"), self.prop_a) {
+            Ok(mut buf) => { buffer.append(&mut buf); },
             Err(e) => { return  Err(e); }
         };
-        match StructEncodeDecode::get_u32(String::from("prop_b"), self.prop_b) {
-            Ok(mut buf) => { buffer. append(&mut buf); },
+        match EncodeTools::get_u32(String::from("prop_b"), self.prop_b) {
+            Ok(mut buf) => { buffer.append(&mut buf); },
+            Err(e) => { return  Err(e); }
+        };
+        match EncodeTools::get_u8_vec(String::from("prop_c"), self.prop_c.clone()) {
+            Ok(mut buf) => { buffer.append(&mut buf); },
+            Err(e) => { return  Err(e); }
+        };
+        match EncodeTools::get_u16_vec(String::from("prop_d"), self.prop_d.clone()) {
+            Ok(mut buf) => { buffer.append(&mut buf); },
             Err(e) => { return  Err(e); }
         };
         Ok(buffer)
@@ -284,6 +477,8 @@ mod tests {
         let mut a: Target = Target {
             prop_a: 9,
             prop_b: 99,
+            prop_c: vec![0, 1, 2, 3, 4],
+            prop_d: vec![5, 6, 7, 8, 9],
         };
         let buf = match a.encode() {
             Ok(buf) => buf,
@@ -296,6 +491,8 @@ mod tests {
         let mut b: Target = Target {
             prop_a: 0,
             prop_b: 0,
+            prop_c: vec![],
+            prop_d: vec![],
         };
         let s = match Storage::new(buf) {
             Ok(s) => s,
@@ -308,6 +505,10 @@ mod tests {
         println!("{:?}", b);
         assert_eq!(a.prop_a, b.prop_a);
         assert_eq!(a.prop_b, b.prop_b);
+        assert_eq!(a.prop_c, b.prop_c);
+        assert_eq!(a.prop_d, b.prop_d);
+        assert_eq!(true, false);
+
     }
 }
 
