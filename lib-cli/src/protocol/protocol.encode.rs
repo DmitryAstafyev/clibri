90e8aaa52916ee40845b2c94e350c05e1af314fd
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-11-14 11:59:46 +0100

    Refactoring

--- lib-cli/src/protocol/protocol.encode.rs
+++ lib-cli/src/protocol/protocol.encode.rs
@@ -1,6 +1,31 @@
 use super::{ sizes };
 use sizes::{ ESize };
 
+fn get_value_buffer(id: u16, size: ESize, mut value: Vec<u8>) -> Result<Vec<u8>, String> {
+    let mut buffer: Vec<u8> = vec!();
+    buffer.append(&mut id.to_le_bytes().to_vec());
+    match size {
+        ESize::U8(size) => {
+            buffer.append(&mut (8 as u8).to_le_bytes().to_vec());
+            buffer.append(&mut size.to_le_bytes().to_vec());
+        },
+        ESize::U16(size) => {
+            buffer.append(&mut (16 as u8).to_le_bytes().to_vec());
+            buffer.append(&mut size.to_le_bytes().to_vec());
+        },
+        ESize::U32(size) => {
+            buffer.append(&mut (32 as u8).to_le_bytes().to_vec());
+            buffer.append(&mut size.to_le_bytes().to_vec());
+        },
+        ESize::U64(size) => {
+            buffer.append(&mut (64 as u8).to_le_bytes().to_vec());
+            buffer.append(&mut size.to_le_bytes().to_vec());
+        },
+    };
+    buffer.append(&mut value);
+    Ok(buffer)
+}
+
 pub trait StructEncode {
 
     fn abduct(&mut self) -> Result<Vec<u8>, String>;
@@ -8,36 +33,11 @@ pub trait StructEncode {
 }
 
 pub trait EnumEncode {
-
-    fn get_value_buffer(&self, id: u16, size: ESize, mut value: Vec<u8>) -> Result<Vec<u8>, String> {
-        let mut buffer: Vec<u8> = vec!();
-        buffer.append(&mut id.to_le_bytes().to_vec());
-        match size {
-            ESize::U8(size) => {
-                buffer.append(&mut (8 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-            ESize::U16(size) => {
-                buffer.append(&mut (16 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-            ESize::U32(size) => {
-                buffer.append(&mut (32 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-            ESize::U64(size) => {
-                buffer.append(&mut (64 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-        };
-        buffer.append(&mut value);
-        Ok(buffer)
-    }
     
     fn abduct(&mut self) -> Result<Vec<u8>, String>;
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
         match self.abduct() {
-            Ok(buf) => self.get_value_buffer(id, ESize::U64(buf.len() as u64), buf.to_vec()),
+            Ok(buf) => get_value_buffer(id, ESize::U64(buf.len() as u64), buf.to_vec()),
             Err(e) => Err(e)
         }
     }
@@ -46,112 +46,87 @@ pub trait EnumEncode {
 
 pub trait Encode {
 
-    fn get_value_buffer(&self, id: u16, size: ESize, mut value: Vec<u8>) -> Result<Vec<u8>, String> {
-        let mut buffer: Vec<u8> = vec!();
-        buffer.append(&mut id.to_le_bytes().to_vec());
-        match size {
-            ESize::U8(size) => {
-                buffer.append(&mut (8 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-            ESize::U16(size) => {
-                buffer.append(&mut (16 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-            ESize::U32(size) => {
-                buffer.append(&mut (32 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-            ESize::U64(size) => {
-                buffer.append(&mut (64 as u8).to_le_bytes().to_vec());
-                buffer.append(&mut size.to_le_bytes().to_vec());
-            },
-        };
-        buffer.append(&mut value);
-        Ok(buffer)
-    }
-
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String>;
 
 }
 
 impl Encode for u8 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::U8_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::U8_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for u16 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::U16_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::U16_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for u32 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::U32_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::U32_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for u64 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::U64_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::U64_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for i8 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::I8_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::I8_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for i16 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::I16_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::I16_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for i32 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::I32_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::I32_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for i64 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::I64_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::I64_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for f32 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::F32_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::F32_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for f64 {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::F64_LEN as u8), self.to_le_bytes().to_vec())
+        get_value_buffer(id, ESize::U8(sizes::F64_LEN as u8), self.to_le_bytes().to_vec())
     }
 }
 
 impl Encode for bool {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
-        self.get_value_buffer(id, ESize::U8(sizes::BOOL_LEN as u8), if self == &true { vec![1] } else { vec![0] })
+        get_value_buffer(id, ESize::U8(sizes::BOOL_LEN as u8), if self == &true { vec![1] } else { vec![0] })
     }
 }
 
 impl Encode for String {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
         let buf = self.as_bytes();
-        self.get_value_buffer(id, ESize::U64(buf.len() as u64), buf.to_vec())
+        get_value_buffer(id, ESize::U64(buf.len() as u64), buf.to_vec())
     }
 }
 
 impl<T> Encode for T where T: StructEncode {
     fn encode(&mut self, id: u16) -> Result<Vec<u8>, String> {
         match self.abduct() {
-            Ok(buf) => self.get_value_buffer(id, ESize::U64(buf.len() as u64), buf.to_vec()),
+            Ok(buf) => get_value_buffer(id, ESize::U64(buf.len() as u64), buf.to_vec()),
             Err(e) => Err(e)
         }
     }
@@ -164,7 +139,7 @@ impl Encode for Vec<u8> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -175,7 +150,7 @@ impl Encode for Vec<u16> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -186,7 +161,7 @@ impl Encode for Vec<u32> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -197,7 +172,7 @@ impl Encode for Vec<u64> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -208,7 +183,7 @@ impl Encode for Vec<i8> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -219,7 +194,7 @@ impl Encode for Vec<i16> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -230,7 +205,7 @@ impl Encode for Vec<i32> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -241,7 +216,7 @@ impl Encode for Vec<i64> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -252,7 +227,7 @@ impl Encode for Vec<f32> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -263,7 +238,7 @@ impl Encode for Vec<f64> {
         for val in self.iter() {
             buffer.append(&mut val.to_le_bytes().to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(len as u64), buffer.to_vec())
     }
 }
 
@@ -275,7 +250,7 @@ impl Encode for Vec<String> {
             buffer.append(&mut (val_as_bytes.len() as u32).to_le_bytes().to_vec());
             buffer.append(&mut val_as_bytes.to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(buffer.len() as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(buffer.len() as u64), buffer.to_vec())
     }
 }
 
@@ -290,6 +265,6 @@ impl<T> Encode for Vec<T> where T: StructEncode {
             buffer.append(&mut (val_as_bytes.len() as u64).to_le_bytes().to_vec());
             buffer.append(&mut val_as_bytes.to_vec());
         }
-        self.get_value_buffer(id, ESize::U64(buffer.len() as u64), buffer.to_vec())
+        get_value_buffer(id, ESize::U64(buffer.len() as u64), buffer.to_vec())
     }
 }
