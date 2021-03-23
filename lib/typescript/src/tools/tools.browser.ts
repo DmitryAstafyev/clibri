9e89d9076743fb3d78bd8202b34431dfcf4ebde2
DmitryAstafyev <dvastafyev@gmail.com>
2021-03-23 11:06:51 +0100

    Add general support of browser

---
+++ lib/typescript/src/tools/tools.browser.ts
@@ -0,0 +1,15 @@
+import { Buffer } from 'buffer';
+
+declare var window: Window | undefined;
+
+const INJECTING_BUFFER_FLAG = '___INJECTING_BUFFER_FLAG___';
+
+export function init() {
+    if (typeof window === 'object' && window !== null) {
+        if ((window as any)[INJECTING_BUFFER_FLAG]) {
+            return;
+        }
+        (window as any).Buffer = Buffer;
+        (window as any)[INJECTING_BUFFER_FLAG] = true;
+    }
+}
